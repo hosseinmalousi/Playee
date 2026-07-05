6d8004c369c09f49e3c1a53b664cece107e49675
hosseinM <hossein.malosi@gmail.com>
2026-07-05 15:59:36 +0200

    added  for enccription dotenv has been added and now the token is hidden

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import logging
+import os
+from dotenv import load_dotenv
 from telegram import Update
 from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
+
+load_dotenv()
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -11,7 +15,8 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
 
 if __name__ == '__main__':
-    application = ApplicationBuilder().token('TOKEN').build()
+    token = os.getenv('TELEGRAM_BOT_TOKEN')
+    application = ApplicationBuilder().token(token).build()
     
     start_handler = CommandHandler('start', start)
     application.add_handler(start_handler)
